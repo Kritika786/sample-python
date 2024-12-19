fdf52c8918ea7b4149852cabe34cc2f5d769d7e7
Kritika786 <73874564+Kritika786@users.noreply.github.com>
2024-12-19 21:39:12 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from flask import flask
 app=app = Flask(__name__)
 @app.route("/")
 def home():
-return "<h1 style='color: red;'>API is running</h1>"
+  return "<h1 style='color: red;'>API is running</h1>"
 if __name__ == "__main__":
  app.run()
 
